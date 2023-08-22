ca445b3586eec0b565742e478cae83186ff22dc5
Adrien Regis <adrien.regis@gmail.com>
2023-08-22 11:07:31 +0200

    Association User + friends

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -13,8 +13,9 @@ class FriendsController < ApplicationController
 
   def create
     @friend = Friend.new(friend_params)
-    if @friend.save!
-      redirect_to friends_path
+    @friend.user = current_user
+    if @friend.save
+      redirect_to root_path
     else
       render :new, status: :unprocessable_entity
     end
@@ -39,4 +40,6 @@ class FriendsController < ApplicationController
   def friend_params
     params.require(:friend).permit(:name, :category, :price, :postal_address, :photo)
   end
+
+
 end
--- app/models/friend.rb
+++ app/models/friend.rb
@@ -2,5 +2,5 @@ class Friend < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
 
-  validates :name, :category, :price, :user_id, presence: true
+  validates :name, :category, :price, presence: true
 end
