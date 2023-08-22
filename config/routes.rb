1bd43dff056afa0e5c18e97a102f39897ea40302
Adrien Regis <adrien.regis@gmail.com>
2023-08-22 13:12:06 +0200

    booking controller only new / create / seed users / application controller: user creation

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :friends
+  resources :friends do
+    resources :bookings, only: [:new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,13 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+User.create!(email: 'toto@gmail.com', password: 'azerty', user_name: "Alphonse", postal_address: "12 rue des Bonnes Vignes")
+User.create!(email: 'roger@gmail.com', password: 'azerty', user_name: "roger", postal_address: "7 rue des Bonnes Vignes")
+User.create!(email: 'alpha@gmail.com', password: 'azerty', user_name: "alpha", postal_address: "10 rue des Bonnes Vignes")
+User.create!(email: 'romeo@gmail.com', password: 'azerty', user_name: "Roméo", postal_address: "25 rue des Bonnes Vignes")
+User.create!(email: 'tango@gmail.com', password: 'azerty', user_name: "November", postal_address: "19 rue des Bonnes Vignes")
+User.create!(email: 'november@gmail.com', password: 'azerty', user_name: "Alphonse", postal_address: "18 rue des Bonnes Vignes")
+
 CATEGORY = %w[sympa cool mechant grincheux dormeur]
 puts 'Creating 10 fake friends...'
 
