9f93c6105fc4651e677c932c84621d742798f0be
Jerodenni <jeromedenni@hotmail.fr>
2023-08-23 11:16:32 +0200

    .env

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -24,10 +24,16 @@ class BookingsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+  end
+
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date)
+    params.require(:booking).permit(:start_date, :end_date, :status)
   end
 
   def set_friend
