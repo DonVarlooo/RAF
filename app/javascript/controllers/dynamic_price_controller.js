dec53b7be5c5b345e0c7a5f30e74b6e1ef84d4eb
Adrien Regis <adrien.regis@gmail.com>
2023-08-24 16:51:26 +0200

    dynamic price working

--- app/javascript/controllers/dynamic_price_controller.js
+++ app/javascript/controllers/dynamic_price_controller.js
@@ -4,7 +4,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ 'startDateInput', 'endDateInput', 'priceHolder' ]
   static values = { price: Number }
-  
+
   connect() {
     console.log("hello le monde");
   }
@@ -25,9 +25,9 @@ export default class extends Controller {
     if (startDate && endDate) {
       const diffTime = (Date.parse(endDate) - Date.parse(startDate))
 
-      let days = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
+      var days = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
     } else {
-      let days = 0
+      var days = 0
     }
       return days + 1
   }
